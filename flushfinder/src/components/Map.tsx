7368c76405c30e2c3d61087e3d94a096b79573a8
Gauri Abeysinghe <g.abeysinghe@student.unsw.edu.au>
2025-03-25 13:28:13 +1100

    added all markers to map

--- flushfinder/src/components/Map.tsx
+++ flushfinder/src/components/Map.tsx
@@ -1,5 +1,8 @@
 import { Key } from "react";
-import { unsw, bathrooms } from '../constants';
+import { unsw, zoom } from '../constants';
+import female_toilets from '../data/bathrooms/female_toilets.json';
+import male_toilets from '../data/bathrooms/male_toilets.json';
+import unisex_toilets from '../data/bathrooms/unisex_toilets.json';
 import { Map, AdvancedMarker, Pin, APIProvider, MapCameraChangedEvent} from "@vis.gl/react-google-maps";
 
 const GM_API_KEY = import.meta.env.VITE_GM_API_KEY;
@@ -11,22 +14,47 @@ function NearMeMap() {
         <APIProvider apiKey={GM_API_KEY} onLoad={() => console.log(`Maps API has loaded`)}>
         <Map
           mapId={GM_MAP_ID}
-          defaultZoom={15}
+          defaultZoom={zoom}
           defaultCenter={unsw}
           onCameraChanged={ (ev: MapCameraChangedEvent) =>
           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
           }
           reuseMaps={true}
         >
-          {bathrooms.map((b: {
-            location_name: string; id: Key; lat: number; lng: number; }) => (
+          {female_toilets.map((b) => (
             <AdvancedMarker 
               key={b.id} 
-              position={{lat: b.lat, lng: b.lng}}
-              onClick={() => console.log(`${b.location_name}`)} // implement show bathrooms here
+              position={b.position}
+              onClick={() => console.log(`${b}`)} // implement show bathrooms here
             >
-              <Pin 
-                background={'#0f9d58'}
+              <Pin // Replace with female icon 
+                background={'#FFC0CB'}
+                borderColor={'#006425'}
+                glyphColor={'#60d98f'}
+              />
+            </AdvancedMarker>
+          ))}
+          {male_toilets.map((b) => (
+            <AdvancedMarker 
+              key={b.id} 
+              position={b.position}
+              onClick={() => console.log(`${b}`)} // implement show bathrooms here
+            >
+              <Pin // replace with male icon
+                background={'#0000FF'}
+                borderColor={'#006425'}
+                glyphColor={'#60d98f'}
+              />
+            </AdvancedMarker>
+          ))}
+          {unisex_toilets.map((b) => (
+            <AdvancedMarker 
+              key={b.id} 
+              position={b.position}
+              onClick={() => console.log(`${b}`)} // implement show bathrooms here
+            >
+              <Pin // replace with unisex icon
+                background={'#00FF00'}
                 borderColor={'#006425'}
                 glyphColor={'#60d98f'}
               />
--- flushfinder/src/constants.tsx
+++ flushfinder/src/constants.tsx
@@ -1,5 +1,5 @@
-export const unsw = {lat: -33.91702648227345, lng: 151.23118166931158};
-export const bathrooms = [
-    { id: 1, location_name: "Ainsworth Building, Ground Floor", lat: -33.918566, lng: 151.231300},
-    { id: 2, location_name: "Business Building, Level 2", lat: -33.91680629996164, lng: 151.2296258973749}
-];
+export const unsw = {
+    lat: -33.91720709737335,
+    lng: 151.23052186838723
+};
+export const zoom = 17;
